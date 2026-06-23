90a3948c2997324afe0e65c264f8cd86d5d6a59c
Lukaowsjnnikowa84-hash <lukaowsjnnikowa84@gmail.com>
2026-06-23 20:50:35 +0300

    бновление сайта

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function openMenu() {
-  document.querySelector('.overlay').style.display = 'block';
-  document.querySelector('.popup-menu').style.display = 'block';
+  document.querySelector(".overlay").style.display = "block";
+  document.querySelector(".popup-menu").style.display = "block";
 }
 
 function closeMenu() {
-  document.querySelector('.overlay').style.display = 'none';
-  document.querySelector('.popup-menu').style.display = 'none';
+  document.querySelector(".overlay").style.display = "none";
+  document.querySelector(".popup-menu").style.display = "none";
 }
-
